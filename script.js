651f8a4f3d985166d7e72e03e37955f59cd9dfd2
Daniel Herrera <danielherrera@Mac.lan>
2025-08-03 22:10:14 -0700

    addition of about.html, new logo, refactoring of divs

---
+++ script.js
@@ -0,0 +1,8 @@
+// script.js
+
+function showContact() {
+    const contactSection = document.getElementById("contact");
+    contactSection.classList.remove("hidden");
+    contactSection.classList.add("block");
+  }
+  
